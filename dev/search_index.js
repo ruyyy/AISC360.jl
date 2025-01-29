fe23af642f5715efbbfd624696234c2f9a08cb7c
Documenter.jl <documenter@juliadocs.github.io>
2025-01-29 20:15:16 +0000

    build based on a69459a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AISC360","category":"page"},{"location":"#AISC360","page":"Home","title":"AISC360","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AISC360.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AISC360]","category":"page"}]
+}
